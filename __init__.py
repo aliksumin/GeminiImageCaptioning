70e5f1fda5476a48a7caa67b1fd78f2b38befb00
aliksumin <whitebirds2009@gmail.com>
2026-02-04 20:24:13 +0100

    feat: Add DatasetFolder node to class and display name mappings.

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@ from .gemini_image_captioning_node import GeminiImageCaptioning
 from .dataset_folder_node import DatasetFolder
 
 NODE_CLASS_MAPPINGS = {
-    "GeminiImageCaptioning": GeminiImageCaptioning
+    "GeminiImageCaptioning": GeminiImageCaptioning,
+    "DatasetFolder": DatasetFolder
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "GeminiImageCaptioning": "Gemini Image Captioning"
+    "GeminiImageCaptioning": "Gemini Image Captioning",
+    "DatasetFolder": "Dataset Folder"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
